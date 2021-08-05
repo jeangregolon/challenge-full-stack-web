7080ae2cb749cc8006f887fb30c9a8568a5d8fc7
Jean Gregolon <jean.gregolon@gmail.com>
2021-08-05 19:22:34 -0300

    Created routes

---
+++ frontend/src/views/Cadastro.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="cadastro">
+    <h1>Cadastro</h1>
+  </div>
+</template>
---
+++ frontend/src/views/Consulta.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="consulta">
+    <h1>Consulta</h1>
+  </div>
+</template>
