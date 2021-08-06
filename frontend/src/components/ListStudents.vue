26ac043537ba70007f5543d1fa37efb4e56b4ee1
Jean Gregolon <jean.gregolon@gmail.com>
2021-08-06 15:18:29 -0300

    Added search to students table

--- frontend/src/components/ListStudents.vue
+++ frontend/src/components/ListStudents.vue
@@ -1,16 +1,33 @@
 <template>
   <v-container>
-    <v-data-table
-      no-data-text="Não há alunos cadastrados."
-      :headers="headers"
-      :items="students"
-      :items-per-page="5"
-    >
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-      </template>
-    </v-data-table>
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-btn rounded dark color="primary" class="ml-2">Cadastrar Aluno</v-btn>
+      </v-card-title>
+
+      <v-data-table
+        :search="search"
+        :headers="headers"
+        :items="students"
+        :items-per-page="5"
+        no-data-text="Não há alunos cadastrados."
+        loading-text="Carregando..."
+      >
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+      </v-data-table>
+    </v-card>
   </v-container>
 </template>
 
@@ -18,14 +35,15 @@
 export default {
   data() {
     return {
+      search: "",
       headers: [
         { text: "RA", value: "id" },
         { text: "NOME", value: "name" },
         { text: "EMAIL", value: "email" },
         { text: "CPF", value: "cpf" },
-        { text: "DATA CRIAÇÃO", value: "createdAt" },
-        { text: "DATA ATUALIZAÇÃO", value: "updatedAt" },
-        { text: "AÇÕES", value: "actions", sortable: false },
+        { text: "DATA DE CRIAÇÃO", value: "createdAt", filterable: false },
+        { text: "ÚLTIMA ATUALIZAÇÃO", value: "updatedAt", filterable: false },
+        { text: "AÇÕES", value: "actions", sortable: false, filterable: false },
       ],
       students: [],
     };
@@ -40,14 +58,16 @@ export default {
       });
     },
     editItem(student) {
-      this.$router.push({ path: `/cadastro/${student.id}` });
+      // this.$router.push({ path: `/cadastro/${student.id}` });
+      console.log("Editar" + student.id);
     },
     deleteItem(item) {
-      this.$http
-        .delete("http://localhost:5000/estudantes/" + item.id)
-        .then(function (data) {
-          this.students = data.body;
-        });
+      // this.$http
+      //   .delete("http://localhost:5000/estudantes/" + item.id)
+      //   .then(function (data) {
+      //     this.students = data.body;
+      //   });
+      console.log("Excluir" + item.id);
     },
   },
 };
--- frontend/src/views/Cadastro.vue
+++ frontend/src/views/Cadastro.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="cadastro">
     <h1 class="display-1 text-center my-3">Cadastro de aluno</h1>
+    <create-students />
   </div>
 </template>
+
+<script>
+import CreateStudents from "../components/CreateStudents.vue";
+
+export default {
+  components: {
+    "create-students": CreateStudents,
+  },
+};
+</script>
