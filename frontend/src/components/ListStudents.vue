06c6c0cb4741fdbf35c889600118db9280051354
Jean Gregolon <jean.gregolon@gmail.com>
2021-08-08 02:54:18 -0300

    Implemented student deletion

--- frontend/src/components/ListStudents.vue
+++ frontend/src/components/ListStudents.vue
@@ -26,10 +26,66 @@
           <v-icon small class="mr-2" @click="editItem(item)">
             mdi-pencil
           </v-icon>
-          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          <v-icon small @click="confirmDelete(item)"> mdi-delete </v-icon>
         </template>
       </v-data-table>
     </v-card>
+    <!-- Delete Dialog start -->
+    <template>
+      <v-dialog v-model="deleteDialog" max-width="680px">
+        <v-card>
+          <v-card-title class="justify-center text-h4">
+            Confirma a exclusão do cadastro de
+          </v-card-title>
+          <v-card-subtitle class="text-center text-h4">
+            {{ studentToDelete.name }} ?
+          </v-card-subtitle>
+          <v-card-actions class="text-center">
+            <v-spacer></v-spacer>
+            <v-btn color="error" @click="deleteItem(studentToDelete)">
+              Confirmar
+            </v-btn>
+            <v-btn color="warning" @click="deleteDialog = false">
+              cancelar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
+    <!-- Delete Dialog end -->
+
+    <!-- Loading Dialog start -->
+    <template>
+      <v-dialog v-model="loadingDialog" persistent max-width="680px">
+        <v-card>
+          <v-card-title class="justify-center text-h4">
+            Excluido cadastro...
+          </v-card-title>
+          <v-card-subtitle class="text-center pt-5">
+            <v-icon x-large class="pr-3">mdi-loading mdi-spin</v-icon>
+          </v-card-subtitle>
+        </v-card>
+      </v-dialog>
+    </template>
+    <!-- Loading Dialog end -->
+
+    <!-- Invalid Dialog start -->
+    <template>
+      <v-dialog v-model="successDeleteDialog" max-width="680px">
+        <v-card>
+          <v-card-title class="justify-center text-h4">
+            Cadastro excluído
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green" text @click="successDeleteDialog = false">
+              OK
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
+    <!-- Invalid Dialog end -->
   </v-container>
 </template>
 
@@ -48,6 +104,10 @@ export default {
         { text: "AÇÕES", value: "actions", sortable: false, filterable: false },
       ],
       students: [],
+      studentToDelete: {},
+      loadingDialog: false,
+      deleteDialog: false,
+      successDeleteDialog: false,
     };
   },
   created() {
@@ -63,13 +123,20 @@ export default {
       // this.$router.push({ path: `/cadastro/${student.id}` });
       console.log("Editar" + student.id);
     },
-    deleteItem(item) {
-      // this.$http
-      //   .delete("http://localhost:5000/estudantes/" + item.id)
-      //   .then(function (data) {
-      //     this.students = data.body;
-      //   });
-      console.log("Excluir" + item.id);
+    confirmDelete(item) {
+      this.studentToDelete = item;
+      this.deleteDialog = true;
+    },
+    deleteItem() {
+      this.deleteDialog = false;
+      this.loadingDialog = true;
+      this.$http
+        .delete("http://localhost:5000/estudantes/" + this.studentToDelete.id)
+        .then(function () {
+          this.loadingDialog = false;
+          this.loadStudents();
+          this.successDeleteDialog = true;
+        });
     },
   },
 };
